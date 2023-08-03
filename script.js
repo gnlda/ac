700f0975f1de32554fe36478e1fbec740d86d8d8
gnlda <136723196+gnlda@users.noreply.github.com>
2023-08-03 20:04:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+const dark0 = document.querySelector("#dark0");
+const dark1 = document.querySelector("#dark1");
+const dark2 = document.querySelector("#dark2");
+const dark3 = document.querySelector("#dark3");
+const dark4 = document.querySelector("#dark4");
+const dark5 = document.querySelector("#dark5");
+const dark6 = document.querySelector("#dark6");
+
+function change() {
+    if (window.getComputedStyle(document.body).backgroundColor == "rgb(14, 14, 14)") {
+        changeImg();
+    } else {
+        returnImg();
+    }
+    
+    function changeImg() {
+        dark0.src = "img/play-dark.svg";
+        dark1.src = "img/arrow-dark.svg";
+        dark2.src = "img/monitor-dark.svg";
+        dark3.src = "img/settings-dark.svg";
+        dark4.src = "img/pen-dark.svg";
+        dark5.src = "img/tv-dark.svg";
+        dark6.src = "img/burger-dark.svg";
+    }
+    
+    function returnImg() {
+        dark0.src = "img/play.svg";
+        dark1.src = "img/arrow.svg";
+        dark2.src = "img/monitor.svg";
+        dark3.src = "img/settings.svg";
+        dark4.src = "img/pen.svg";
+        dark5.src = "img/tv.svg";
+        dark6.src = "img/burger.svg";
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    change();
+    setInterval(change, 2000)
+})
+
+const burger = document.querySelector(".burger-btn");
+const menu = document.querySelector(".burger-menu");
+const body = document.body;
+
+burger.addEventListener("click", burgerHandler);
+window.addEventListener("resize", close);
+
+function close() {
+    const width = window.innerWidth;
+    if (width > 949) {
+        menu.classList.remove("open");
+        body.classList.remove("noscroll");
+    }
+}
+
+function burgerHandler(e) {
+    e.preventDefault();
+    menu.classList.toggle("open");
+    body.classList.toggle("noscroll");
+}
